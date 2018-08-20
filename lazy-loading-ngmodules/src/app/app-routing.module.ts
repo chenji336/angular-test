902a43f64a0e2612691339525a41c3eaf5a9a2ff
chenji336 <1010072738@qq.com>
2018-08-20 10:11:21 +0800

    first commit

---
+++ lazy-loading-ngmodules/src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+
+const routes: Routes = [
+  {
+    path: 'customers',
+    loadChildren: 'app/customers/customers.module#CustomersModule'
+  },
+  {
+    path: 'orders',
+    loadChildren: 'app/orders/orders.module#OrdersModule'
+  },
+  
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule],
+  providers: []
+})
+export class AppRoutingModule { }
